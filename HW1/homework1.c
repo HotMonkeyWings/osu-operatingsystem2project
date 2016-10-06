afa6edf92fef7750844b858e4b98f7f42c96a841
Jonathan Harijanto <harijanj@os-class.engr.oregonstate.edu>
2016-10-05 17:44:46 -0700

    add mt.h and modify homework1.c

--- HW1/homework1.c
+++ HW1/homework1.c
@@ -8,34 +8,46 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
 
-#include <mt19937ar.c>
-
+#include "mt19937ar.c"
 
 // Set buffer size 
-int buffIndex = 32;
+#define BUFFINDEX = 32;
 
 // Condition variable initialization
-pthread_cond_t bufferFull;
-pthread_cont_t bufferNotFull;
+pthread_cond_t bufferFull =  PTHREAD_COND_INITIALIZER;
+pthread_cond_t bufferNotFull = PTHREAD_COND_INITIALIZER;
 
 // Struct with two numbers
-struct value {
+struct value{
 	int number;
 	int waitPeriod;
 };
 
 void *producer(void *arg){
-
+	
 	// Wait random amount of time (3-7 seconds)
-	int producerWait = rand() % (7 + 1 - 3) + 3;
+	int producerWait = genrand_int32() % 8 + 3;	
 
+	
+	while(true){
+
+	}
 }
 
 void *consumer(void *arg){
 
+	while(true){
+
+	}
 }
 
 int main(){
+	
+	// Initialize seed with time()
+	init_genrand(time(NULL));	
 
+	struct value buffer[BUFFINDEX];
+	
 } 
