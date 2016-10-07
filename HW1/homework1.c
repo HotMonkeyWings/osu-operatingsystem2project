eab07e7154a3fca54a2e203296fcd3dcb8769c4c
Jonathan Harijanto <harijanj@os-class.engr.oregonstate.edu>
2016-10-06 21:32:08 -0700

    producer part

--- HW1/homework1.c
+++ HW1/homework1.c
@@ -4,7 +4,8 @@
  *
  *	Resources: 
  *	www.sourceware.org, docs.oracle.com, https://linux.die.net
- *	pubs.opengroup.org  
+ *	pubs.opengroup.org, stackoverflow.com, ibm.com/support/knowledgecenter
+ *
  *
  */
 
@@ -17,44 +18,80 @@
 #include "mt19937ar.c"
 
 // Set buffer size 
-#define BUFFINDEX = 32;
+#define BUFFERMAX 32
+
+int bufferCounter = 0;
 
 // Condition variable initialization
-pthread_cond_t bufferFull =  PTHREAD_COND_INITIALIZER;
-pthread_cond_t bufferNotFull = PTHREAD_COND_INITIALIZER;
+pthread_cond_t producerWait;
+pthread_cond_t consumerWait;
 
 // Insert something here
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex;
 
 // Struct with two numbers
-struct value{
+struct value {
 	int number;
 	int waitPeriod;
 };
 
+// bla
+struct value buffer[BUFFERMAX];
+
 void *producer(void *arg){
-	
+	// Declare struct
+	struct value producer;
+
 	// Wait random amount of time (3-7 seconds)
 	int producerWait = genrand_int32() % 8 + 3;	
-
 	
-	while(true){
+	while(1){
 		// Initial wait time before 'producing' 
 		sleep(producerWait);
+		
 		// Block calling thread to ensure synchronized access
 		pthread_mutex_lock(&mutex);
-		// lll
-		while(){
+	
+		// Check for space availability in buffer
+		while(bufferCounter == BUFFERMAX){
+			printf("Buffer is full, producer has to wait.\n");
+	
+			// Condition variable is blocked - producer has to wait
+			pthread_cond_wait(&producerWait,&mutex); 
+		}
+	
+		// If buffer not full, Producer generate random number value 
+		// 1 - 25 
+		producer.number = genrand_int32() % 25 + 1;
+	
+		// Producer random wait time (3-7) before producing again
+		producer.waitPeriod = genrand_int32() % 8 + 3;
+	
+		// Store it to buffer
+		buffer[bufferCounter] = producer;
 
-		}			
+		printf("I'm a producer!\n");
+		printf("Current index: %d/32, Value of produced item: %d," 
+			"Going to sleep for: %d seconds\n", bufferCounter, 
+			producer.number, producer.waitPeriod);
+
+		if(bufferCounter == 32) {
+			printf("Resetting buffer...\n");
+			bufferCounter = 0;
+		} else {
+			bufferCounter += 1;		
+		}
+	
 		// Unlock the thread
 		pthread_mutex_unlock(&mutex);
+		// Wakes up the waiting consumer
+		pthread_cond_signal(&consumerWait);
 	}
 }
 
 void *consumer(void *arg){
 
-	while(true){
+	while(1){
 
 	}
 }
@@ -63,7 +100,5 @@ int main(){
 	
 	// Initialize seed with time()
 	init_genrand(time(NULL));	
-
-	struct value buffer[BUFFINDEX];
 	
 } 
