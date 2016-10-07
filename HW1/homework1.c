496310f187be10b620098e8682c8d49075c6394b
Jonathan Harijanto <harijanj@os-class.engr.oregonstate.edu>
2016-10-06 23:46:27 -0700

    group work late night session

--- HW1/homework1.c
+++ HW1/homework1.c
@@ -2,10 +2,10 @@
  *	CS 444 Assignment 1 - The Producer-Consumer Problem
  *	By: Jonathan Harijanto and Kyle Martin Collins
  *
- *	Resources: 
+ *	Resources:  
  *	www.sourceware.org, docs.oracle.com, https://linux.die.net
- *	pubs.opengroup.org, stackoverflow.com, ibm.com/support/knowledgecenter
- *
+ *	pubs.opengroup.org, stackoverflow.com, ibm.com/support/knowledgecenter,
+ *	computing.llnl.gov, yolinux.com, and classmates.
  *
  */
 
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <signal.h>
 
 #include "mt19937ar.c"
 
@@ -57,7 +58,7 @@ void *producer(void *arg){
 			printf("Buffer is full, producer has to wait.\n");
 	
 			// Condition variable is blocked - producer has to wait
-			pthread_cond_wait(&producerWait,&mutex); 
+			pthread_cond_wait(&producerWait, &mutex); 
 		}
 	
 		// If buffer not full, Producer generate random number value 
@@ -75,6 +76,7 @@ void *producer(void *arg){
 			"Going to sleep for: %d seconds\n", bufferCounter, 
 			producer.number, producer.waitPeriod);
 
+		// Check buffer space after storage
 		if(bufferCounter == 32) {
 			printf("Resetting buffer...\n");
 			bufferCounter = 0;
@@ -92,13 +94,63 @@ void *producer(void *arg){
 void *consumer(void *arg){
 
 	while(1){
+		// Block calling thread to ensure synchronized access
+		pthread_mutex_lock(&mutex);
+		
+		// Check for empty space in the buffer
+		while(bufferCounter == 0){
+			printf("Buffer is empty, consumer has to wait!");
+			// Condition variable is blocked, consumer has to wait
+			pthread_cond_wait(&consumerWait, &mutex);
+		}
 
+		// If the buffer is not empty - Consumer consume the value
+
+		// ** DO SOMETHING **
+
+
+		// Unlock the thread
+		pthread_mutex_unlock(&mutex);
+		pthread_cond_signal(&producerWait);
+	}
+}
+
+// A (popular) function that will terminate the process if receive SIGINT signal
+void signalHandler(int signalNumber){
+	if(signalNumber == SIGINT){
+		printf("Received SIGINT (CTRL +C)\n");
+		// Sends a cancelation request to the threads
+		//
+		// ** DO SOMETHING **
 	}
 }
 
 int main(){
+
+	// Catch interruption
+	signal(SIGINT, signalHandler);
 	
+	// Keep a thread ID
+	pthread_t producer;
+	pthread_t consumer;
+
+	// Buffer memory allocation
+	// ** DO SOMETHING **
+
 	// Initialize seed with time()
 	init_genrand(time(NULL));	
+
+	// Initialize mutex and variable condition
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&producerWait, NULL);
+	pthread_cond_init(&consumerWait, NULL);
+
+	// Starts new threads
+	pthread_create(&producer, NULL, producer, NULL);
+	pthread_create(&consumer, NULL, consumer, NULL);
+
+	// Suspend execution of the calling thread until target terminated
+	pthread_join(producer, NULL);
+	pthread_join(consumer, NULL);
 	
 } 
